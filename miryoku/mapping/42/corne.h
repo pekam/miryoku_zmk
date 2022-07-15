12002a1bc19bfa97efda932d62f674e401ec032e
Pekka Maanpää <pekka.maanpaa@gmail.com>
2022-07-15 16:49:56 +0300

    Add bluetooth and reset controls

--- miryoku/mapping/42/corne.h
+++ miryoku/mapping/42/corne.h
@@ -11,10 +11,10 @@
      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 ) \
-XXX  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
-XXX  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  XXX \
-               K32  K33  K34       K35  K36  K37
+&bt BT_CLR  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &bt BT_NXT \
+XXX         K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  &kp SQT \
+&reset      K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &reset \
+                      K32  K33  K34       K35  K36  K37
 
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTPINKIE 2 3
 #define MIRYOKU_KLUDGE_TOPROWCOMBOS_LEFTINNERINDEX 3 4
